65a807e565479ab3ab1fe104ec9c8730a4a3360c
kesingleton2021 <kesingleton2021@gmail.com>
2023-06-28 16:51:45 -0400

    6/28/23

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello world'
+
+@app.route('/', methods=['POST'])
+def anything():
+    request_body = request.get_json()
+
+    return request_body
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
