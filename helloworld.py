7fa1b2381a69dd7115b7b3b9ea4c8ab7c60745b1
kesingleton2021 <kesingleton2021@gmail.com>
2023-07-05 17:11:27 -0400

    7/5/23

--- helloworld.py
+++ helloworld.py
@@ -1,16 +1,24 @@
 from flask import Flask, request
-
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello world'
 
-@app.route('/', methods=['POST'])
-def anything():
+@app.route('/echo', methods=['POST'])
+def echo():
     request_body = request.get_json()
-
     return request_body
+
+
+@app.route('/proxy')
+def proxy():
+    r = requests.get('https://pokeapi.co/api/v2/pokemon/eevee',auth=('user','pass'))
+    print(r.status_code)
+    print(r.headers['content-type'])
+
+    return f'{r.status_code}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
